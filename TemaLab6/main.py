0b2fa8c2520528bb8d75e54077b6f6c482792010
Elena Caldare <elenacaldare2509@gmail.com>
2023-11-07 11:39:46 +0200

    Tema Lab6

--- TemaLab6/main.py
+++ TemaLab6/main.py
@@ -6,25 +6,19 @@ import arviz as az
 Y_values = [0, 5, 10]
 theta_values = [0.2, 0.5]
 
-# Define observed data
 observed_data = np.array(Y_values)
 
-# Create a PyMC model
 model = pm.Model()
 
 with model:
-    # Prior for n, Poisson distribution with mean 10
     n = pm.Poisson("n", mu=10)
 
-    # Likelihood, binomial distribution for Y given θ
     for Y in Y_values:
         for theta in theta_values:
             Y_obs = pm.Binomial(f"Y_obs_{Y}_{theta}", n=n, p=theta, observed=Y)
 
-    # Bayesian inference
-    step = pm.Metropolis()  # You can choose a different step method if needed
+    step = pm.Metropolis()
     trace = pm.sample(2000, tune=1000, step=step, cores=1)
 
-# Visualize results
 az.plot_posterior(trace)
 plt.show()
